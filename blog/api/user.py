237b484d80c4560caceb23dca21ed0eb1953ef42
Peka97 <ivan.karasyov97@gmail.com>
2023-04-18 13:27:11 +0000

    Lesson_9

---
+++ blog/api/user.py
@@ -0,0 +1,39 @@
+from flask_combo_jsonapi import ResourceDetail, ResourceList
+
+from blog.models.database import db
+from blog.schemas import UserSchema
+from blog.models import User
+from blog.schemas import AuthorSchema
+from blog.models import Author
+
+
+class UserList(ResourceList):
+    schema = UserSchema
+    data_layer = {
+        "session": db.session,
+        "model": User,
+    }
+
+
+class UserDetail(ResourceDetail):
+    schema = UserSchema
+    data_layer = {
+        "session": db.session,
+        "model": User,
+    }
+
+
+class AuthorList(ResourceList):
+    schema = AuthorSchema
+    data_layer = {
+        "session": db.session,
+        "model": Author,
+    }
+
+
+class AuthorDetail(ResourceDetail):
+    schema = AuthorSchema
+    data_layer = {
+        "session": db.session,
+        "model": Author,
+    }
